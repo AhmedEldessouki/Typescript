7c367c5d7c9c13f9518d5f272baf4c94cbf9ce4c
Nemo <Nemo@NEMO>
2020-07-03 17:15:21 +0300

    learning
    interface and classes

---
+++ cutomeTypes.ts
@@ -0,0 +1,19 @@
+//interfaces always strat with a capital letter (cant use Algorithms with)
+// interface Point {
+//   x: number;
+//   y: number;
+// }
+// can use algorithms with classes -- Cohesion
+class Point {
+  //fields
+  x: number;
+  y: number;
+  //metha
+  draw() {
+    //logic
+  }
+
+  getDistance(another: Point) {
+    //logic
+  }
+}
